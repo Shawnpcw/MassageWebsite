8547ad8ede15d6fa590f5ae1b397c92f822b9666
Shawnpcw <s.cannon.williams@gmail.com>
2018-09-24 10:47:28 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { PriceComponent } from './price/price.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch: "full", redirectTo:"home"},
+  {path:"home", component:HomeComponent},
+  {path:"contact", component:ContactComponent},
+  {path:"policies", component:PoliciesComponent},
+  {path:"prices", component:PriceComponent},
+  {path:"appointments", component:AppointmentsComponent},
+  {path:"**", redirectTo: "home"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentsComponent,
+    ContactComponent,
+    HomeComponent,
+    PoliciesComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
