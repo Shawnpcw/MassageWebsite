3a3b31c52b1ea660a5cd66efc8b84d4a1e54b0c8
Shawnpcw <s.cannon.williams@gmail.com>
2018-09-24 11:19:16 -0700

    working jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '../../node_modules/@angular/router';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  constructor(private router:Router){}
+  home(){
+    this.router.navigate(['/home'])
+  }
+  contact(){
+    this.router.navigate(['/contact'])
+  }
+  policies(){
+    this.router.navigate(['/policies'])
+  }
+  appointments(){
+    this.router.navigate(['/appointments'])
+  }
+  prices(){
+    this.router.navigate(['/prices'])
+  }
+
 }
